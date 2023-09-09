30433d106112122f67ab84db346def7bf19426ff
PC <kechkarayoub@gmail.com>
2023-09-09 09:23:55 +0100

    revert initial configuration

--- healthkools/metro.config.js
+++ healthkools/metro.config.js
@@ -9,23 +9,3 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-/*module.exports = mergeConfig((async () => {
-    const {
-      resolver: { sourceExts, assetExts },
-    } = await getDefaultConfig(__dirname);
-    return {
-      transformer: {
-        getTransformOptions: async () => ({
-          transform: {
-            experimentalImportSupport: false,
-            inlineRequires: false,
-          },
-        }),
-      },
-      resolver: {
-        assetExts: assetExts.filter(ext => ext !== 'svg'),
-        sourceExts: [...sourceExts, 'jsx', 'js', 'json', 'ts', 'tsx', 'svg'],
-      },
-    };
-  })());*/
