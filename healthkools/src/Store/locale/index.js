b57d0020aab31123d511a4e1ac0f4fce9f857587
kechkarayoub <kechkarayoub@gmail.com>
2023-09-17 06:40:21 -0700

    add EncryptedStorage library instead of RNSecureStorage.

--- healthkools/src/Store/locale/index.js
+++ healthkools/src/Store/locale/index.js
@@ -2,7 +2,8 @@
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getLocales } from "react-native-localize";
 import {supported_languages} from 'src/config/global';
-import RNSecureStorage, { ACCESSIBLE } from 'rn-secure-storage'
+import EncryptedStorage from 'react-native-encrypted-storage';
+
 
 // const locale_storage = new Storage({
 //   // maximum capacity, default 1000 key-ids
@@ -60,39 +61,57 @@ import RNSecureStorage, { ACCESSIBLE } from 'rn-secure-storage'
 //   }
 // });
 
-export const set = (key, val, expires) => {
-  RNSecureStorage.set(key, val, {accessible: ACCESSIBLE.WHEN_UNLOCKED}).then((res) => {
-    console.log(res);
-    }, (err) => {
-    console.log(err);
-    });
+export const set = async (key, value) => {
+  try {
+      await EncryptedStorage.setItem(key, value);
+
+      // Congrats! You've just stored your first value!
+  } catch (error) {
+      // There was an error on the native side
+      console.log(error.code); 
+  }
 }
+
 export const get = async (key, callback) => {
-    RNSecureStorage.get(key).then((value) => {
-      if(!value && key == "locale_language"){
-        var value = "fr";
-        let locale_language = getLocales()[0];
-        var locale_splited = locale_language && locale_language.languageTag && locale_language.languageTag.split("-")[0];
-        if(locale_splited && supported_languages.indexOf(locale_splited) !== -1){
-          value = locale_splited;
+  try {   
+      await EncryptedStorage.getItem(key).then(value => {
+        if(!value && key == "locale_language"){
+          var value = "fr";
+          let locale_language = getLocales()[0];
+          var locale_splited = locale_language && locale_language.languageTag && locale_language.languageTag.split("-")[0];
+          if(locale_splited && supported_languages.indexOf(locale_splited) !== -1){
+            value = locale_splited;
+          }
         }
-      }
-      if(callback){
-        callback(value);
-      }
-      return value;
-    }).catch((err) => {
-    console.log(err)
-    });
+        if(callback){
+          callback(value);
+        }
+        return value;
+      });
+  
+  } catch (error) {
+      // There was an error on the native side
+      console.log(error.code); 
+  }
 }
 
-export const remove = (key) => {
-  RNSecureStorage.remove(key).then((val) => {
-    console.log(val)
-    }).catch((err) => {
-    console.log(err)
-    });
+export const remove = async (key) => {
+  try {
+      await EncryptedStorage.removeItem(key, value);
+
+      // Congrats! You've just stored your first value!
+  } catch (error) {
+      // There was an error on the native side
+      console.log(error.code); 
+  }
+}
+export const clear = async () => {
+  try {
+    await EncryptedStorage.clear();
+
+  } catch (error) {
+      // There was an error on the native side
+      console.log(error.code); 
+  }
 }
 
-
-export default RNSecureStorage;
