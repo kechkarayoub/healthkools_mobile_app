04c7879e4c4f9cfa51e9db98716a48d8cf5a8ddb
PC <kechkarayoub@gmail.com>
2023-09-17 22:54:30 +0100

    debug font awesome icons

--- healthkools/src/Components/FormFields/CustomTouchableOpacityWithIcon.js
+++ healthkools/src/Components/FormFields/CustomTouchableOpacityWithIcon.js
@@ -3,7 +3,9 @@ import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { Stack, IconButton } from "@react-native-material/core";
 import Icon from "react-native-vector-icons/FontAwesome";
-import {Ionicons,MaterialCommunityIcons,FontAwesome5} from "react-native-vector-icons/FontAwesome";
+import FontAwesomeIcon from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 const BUTTON_SIZE = 30
 
 class CustomTouchableOpacityWithIcon extends React.Component {
@@ -34,7 +36,7 @@ class CustomTouchableOpacityWithIcon extends React.Component {
         onPress={this.props.onPress} testID={this.props.test_id}
       >
         <Text style={[styles.textStyle, this.props.textStyle || {}, text_length == 4 ? {fontSize: 17} : text_length == 5 ? {fontSize: 14} : {}]}>{this.props.text}</Text>
-        <Ionicons
+        <Icon
           name={this.props.icon_name}
           size={16}
           style={styles.iconStyle}
