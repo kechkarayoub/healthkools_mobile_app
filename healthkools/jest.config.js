b081ac281c5e753ee1a6c3f44852a5a07c7772c7
PC <kechkarayoub@gmail.com>
2023-10-26 08:59:42 +0100

    - add act to asynchronous event in CustomCountriesSelect.test.js - set maxWorkers to 1 in jest.config.js

--- healthkools/jest.config.js
+++ healthkools/jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|react-native-vector-icons|@react-native))/',
   ],
+  maxWorkers: 1,
   // collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
 };
