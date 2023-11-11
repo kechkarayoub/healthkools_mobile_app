ec201a02da15ffc5e296994f9e809f6d59d5414b
PC <kechkarayoub@gmail.com>
2023-11-11 09:07:16 +0100

    replace create store with configurestore

--- healthkools/src/Store/configureStore.js
+++ healthkools/src/Store/configureStore.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
-import gloabalReducer from 'src/Store/Reducers/globalReducer'
+import gloabalReducer from 'src/Store/Reducers/globalReducer';
+import { configureStore } from '@reduxjs/toolkit';
+
 
 // const gg = 5;
 // gg = 6; 
-export default createStore(gloabalReducer);
+const store = configureStore({
+    reducer: gloabalReducer,
+    // other configuration options if needed
+});
+  
+export default store;
+ 
